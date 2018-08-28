fbfaff3a8c11a83272f9fe5edddbbca1589b26d1
HLoveMe <2436422656@qq.com>
2018-08-29 01:06:41 +0800

    keyboard布局

--- src/app/Provider/Component/KeyBorads/VinKeyBoard/VinKeyBoard.ts
+++ src/app/Provider/Component/KeyBorads/VinKeyBoard/VinKeyBoard.ts
@@ -2,7 +2,14 @@ import { Component, Input,OnInit,Renderer2, ViewChild,ElementRef} from "@angular
 import { Platform } from 'ionic-angular';
 import { CustomAnimation } from './../../../Animations/KeyBoard';
 
-
+class KeyValue{
+    key:any = ""
+    value:string =""
+    constructor(key,value){
+        this.key = key;
+        this.value = value;
+    }
+}
 
 @Component({
     selector:"vinkeyboard",
@@ -15,6 +22,26 @@ export class VinKeyBoard implements OnInit{
     isIphoneX:boolean = false
     @Input() state:string="inactive"
     @ViewChild("Container") Container:ElementRef
+    Voice:KeyValue = {key:"V",value:""}
+    Delete:KeyValue = {key:"D",value:""}
+    keyValueCollection:Array<Array<KeyValue>> = [
+        [
+            {key:"1",value:""},{key:"2",value:""},{key:"3",value:""},{key:"4",value:""},{key:"5",value:""},{key:"6",value:""}
+            ,{key:"7",value:""},{key:"8",value:""},{key:"9",value:""},{key:"0",value:""}
+        ],
+        [
+            {key:"Q",value:""},{key:"W",value:""},{key:"E",value:""},{key:"R",value:""},{key:"T",value:""},{key:"Y",value:""}
+            ,{key:"U",value:""},{key:"I",value:""},{key:"O",value:""},{key:"P",value:""}
+        ],
+        [
+            {key:"A",value:""},{key:"S",value:""},{key:"D",value:""},{key:"F",value:""},
+            {key:"G",value:""},{key:"H",value:""},{key:"J",value:""},{key:"K",value:""},{key:"L",value:""}
+        ],
+        [
+            {key:"Z",value:""},{key:"X",value:""},{key:"C",value:""},{key:"V",value:""},
+            {key:"B",value:""},{key:"N",value:""},{key:"M",value:""}
+        ]
+    ]
     constructor (
         private plt:Platform,
         private render:Renderer2
