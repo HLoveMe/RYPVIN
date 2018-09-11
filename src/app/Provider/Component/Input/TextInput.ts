23be7725a4a2b398cb48a2c1abfb50df04c9c16a
RYP.MAC <2436422656@qq.com>
2018-09-11 21:06:47 +0800

    textI

--- src/app/Provider/Component/Input/TextInput.ts
+++ src/app/Provider/Component/Input/TextInput.ts
@@ -1,35 +1,76 @@
 
-import { Component, Output,Renderer2, Input,EventEmitter, ViewChild,ElementRef } from "@angular/core";
+import { Component, Output, Renderer2, Input, EventEmitter, ViewChild, ElementRef, ChangeDetectorRef, forwardRef } from "@angular/core";
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
-class IconOptions{
-    name:string = ""
-    fontSize:string = "3rem"
+class IconOptions {
+    name: string = ""
+    fontSize: string = "16px"
 }
-class ExIconOption extends IconOptions{
-    selectIcon:string = ""
-    status:boolean = false
-    constructor(ops){
+//icon 切换
+class ExIconOption extends IconOptions {
+    selName: string = ""
+    select: boolean = false
+    constructor(ops) {
+        super()
+    }
+}
+//icon 显示和隐藏
+class ShowIconOptopn extends IconOptions {
+    show: boolean = true
+    constructor(ops) {
         super()
     }
 }
 
+const RADIO_VALUE_ACCESSOR: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => TextInput),
+    multi: true
+};
+export function NoValidate(text){
+    return text
+}
+
+export type Validate = Function
+
 @Component({
     selector: "text-input",
     templateUrl: "TextInput.html",
-    styles:[`:host{display:block;overflow: hidden;}`],
+    styles: [`:host{display:block;overflow: hidden;}`],
+    providers: [RADIO_VALUE_ACCESSOR],
 })
-export class TextInput {
-    @Input() leftIcons:Array<IconOptions> = []
-    @Output() iconClick:EventEmitter<any> = new EventEmitter()
-    @ViewChild("Container") Container:ElementRef;
-    constructor(private render:Renderer2) { 
-        
+export class TextInput implements ControlValueAccessor {
+    value: string = "1111"
+    _registerOnChange: Function
+    @Input() leftIcons: Array<IconOptions> = []
+    @Input() rightIcons: IconOptions[] = []
+    @Input() Validate:Validate = NoValidate
+    @Output() iconClick: EventEmitter<any> = new EventEmitter()
+    @ViewChild("Container") Container: ElementRef;
+    constructor(private render: Renderer2, private dete: ChangeDetectorRef) { }
+    update() {
+        this.dete.markForCheck()
     }
-    IconClick(item){
-        this.iconClick.next(item)
+    IconClick(item) {
+        this.iconClick.emit(item)
     }
     ngAfterViewInit() {
-        let rect = (this.Container.nativeElement as HTMLElement).getBoundingClientRect() as DOMRect
-        this.render.setStyle(this.Container.nativeElement,"border-radius",rect.height/2 +"px")
+        let rect = (this.Container.nativeElement as HTMLElement).getBoundingClientRect()
+        this.render.setStyle(this.Container.nativeElement, "border-radius", rect.height / 2 + "px")
     }
+    textChange(text) {
+        this._registerOnChange && this._registerOnChange(text)
+    }
+    //ControlValueAccessor 协议
+    writeValue(obj: string) {
+        if(this.Validate){
+            obj = this.Validate(obj)
+        }
+        this.value = obj
+    }
+    registerOnChange(fn: Function) {
+        this._registerOnChange = fn
+    }
+    registerOnTouched(fn: any) { }
+    setDisabledState(isDisabled: boolean) { }
 }
--- src/pages/User/UserLogin/UserLogin.ts
+++ src/pages/User/UserLogin/UserLogin.ts
@@ -1,5 +1,6 @@
 
-import { Component } from "@angular/core";
+import { Component, Input, ViewChild } from "@angular/core";
+import { TextInput } from "../../../app/Provider/Component/Input/TextInput";
 
 @Component({
     selector:"user-login",
@@ -9,18 +10,42 @@ export class UserLogin{
     passLIcons = [
         {
             name:"ios-contacts",
-            fontSize:"3rem",
+            fontSize:"0.6rem",
+        }
+    ]
+    accountRIcons = [
+        {
+            name:"ios-close-circle-outline",
+            fontSize:"0.3rem",
+            show:true
         }
     ]
     acountLIcon = [
         {
             name:"ios-phone-portrait-outline",
-            fontSize:"3rem",
+            fontSize:"0.6rem",
         }
     ] 
+    @ViewChild("Account") Account:TextInput
+    @ViewChild("Password") Password:TextInput
     constructor(){}
-    iconClick(item){
-        console.log(item);
+    AccounticonClick(item){
+        // if(item.show != null){
+        //     item.show = !item.show
+        //     this.Account.update()
+        // }else if(item.select != null){
+        //     item.select = !item.select
+        //     this.Account.update()
+        // }
+    }
+    PasswordiconClick(item){
+        // if(item.show != null){
+        //     item.show = !item.show
+        //     this.Password.update()
+        // }else if(item.select != null){
+        //     item.select = !item.select
+        //     this.Password.update()
+        // }
     }
 }
     
